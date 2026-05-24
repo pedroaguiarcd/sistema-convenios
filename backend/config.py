bb8c89704a24eee26692c54d281b85684918ab62
Pedro Aguiar <j.pedroaguiar25@gmail.com>
2026-05-24 17:47:06 -0300

    fix: fix config

--- backend/config.py
+++ backend/config.py
@@ -1,6 +1,6 @@
 class Config:
     SQLALCHEMY_DATABASE_URI = (
-        "mysql+pymysql://root:@localhost/convenios_db"
+      "mysql+pymysql://convenios_user:123456@localhost/convenios_db"
     )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
